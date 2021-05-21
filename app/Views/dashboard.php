71f94fb89ee30fb38b0b9302f1cd4d2c5a8ac550
motinho311 <issamohammed311@gmail.com>
2021-05-21 21:15:58 +0300

    comit

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -1,3 +1,8 @@
-<div class="top-bar d-block p-2 ">
-   
+<div class="top-bar d-block p-2">
+    <div id="mySidebar" class="sidebar">
+        <a href="#home"><i class="fa fa-fw fa-home"></i> Home</a>
+        <a href="#services"><i class="fa fa-fw fa-wrench"></i> Services</a>
+        <a href="#clients"><i class="fa fa-fw fa-user"></i> Clients</a>
+        <a href="#contact"><i class="fa fa-fw fa-envelope"></i> Contact</a>
+    </div>
 </div>
--- app/Views/login.php
+++ app/Views/login.php
@@ -1,7 +1,11 @@
 <div class="main-div">
-	  	  <h3>AUTHORIZATION</h3><br>
-	  	<input type="policeid"  placeholder="Police_ID"/>
-	  	<input type="password"  placeholder="password"/>
- 
- <button  type="submit" class="btn">Accessing</button><br>
+    <h3>AUTHORIZATION</h3><br>
+    <div class="ch">
+        <form action="" >
+            <input type="policeid" id="policeId" placeholder="Police_ID"/>
+            <input type="password" id="password" placeholder="Password"/>
+        </form>
+        
+    </div> 
+ <button  type="submit" class="btn">Accessing</button><br> 
 </div>
