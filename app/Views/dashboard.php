b244b208cf10a1bf529a6fe722bd22e78063e3f9
motinho311 <issamohammed311@gmail.com>
2021-05-22 01:52:03 +0300

    comit

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -1,8 +1,10 @@
 <div class="top-bar d-block p-2">
     <div id="mySidebar" class="sidebar">
+        <div class='log'>
         <a href="#home"><i class="fa fa-fw fa-home"></i> Home</a>
         <a href="#services"><i class="fa fa-fw fa-wrench"></i> Services</a>
         <a href="#clients"><i class="fa fa-fw fa-user"></i> Clients</a>
         <a href="#contact"><i class="fa fa-fw fa-envelope"></i> Contact</a>
+</div>
     </div>
 </div>
