0230e434511bc8166d4d9572012e625b15dbe1da
motinho311 <issamohammed311@gmail.com>
2021-05-21 21:04:56 +0300

    some changes

--- app/Views/login.php
+++ app/Views/login.php
@@ -1,8 +1,7 @@
 <div class="main-div">
-	  	<h3>AUTHORIZATION</h3><br>
+	  	  <h3>AUTHORIZATION</h3><br>
 	  	<input type="policeid"  placeholder="Police_ID"/>
 	  	<input type="password"  placeholder="password"/>
  
  <button  type="submit" class="btn">Accessing</button><br>
-   
 </div>
--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -1,19 +1,5 @@
 
 
 
-    <script>
-        function openNav() {
-            document.getElementById("mySidebar").style.width = "250px";
-            document.getElementById("main").style.marginLeft = "250px";
-        }
-
-        function closeNav() {
-            document.getElementById("mySidebar").style.width = "0";
-            document.getElementById("main").style.marginLeft= "0";
-            
-        }
-        <div class="footer">
-			<footer><p>Copyright &copy; Tallog Tech </p></footer>
-   </div>
 </body>
 </html>
