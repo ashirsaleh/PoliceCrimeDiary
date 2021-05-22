506cbe2a5eb192a9aa094444745e19634033affd
motinho311 <issamohammed311@gmail.com>
2021-05-22 16:34:32 +0300

    fiftcomit

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -1,10 +1,12 @@
 <div class="top-bar d-block p-2">
     <div id="mySidebar" class="sidebar">
         <div class='log'>
-        <a href="#home"><i class="fa fa-fw fa-home"></i> Home</a>
-        <a href="#services"><i class="fa fa-fw fa-wrench"></i> Services</a>
-        <a href="#clients"><i class="fa fa-fw fa-user"></i> Clients</a>
-        <a href="#contact"><i class="fa fa-fw fa-envelope"></i> Contact</a>
-</div>
+            <a href="#home"><i class="fa fa-fw fa-home"></i> Home</a>
+            <a href="#dasboard"><i class="fa fa-fw fa-dashboard"></i>Dashboard</a>
+            <a href="#form"><i class="fa fa-fw fa-document"></i>Form</a>
+            <a href="#services"><i class="fa fa-fw fa-wrench"></i> Services</a>
+            <a href="#clients"><i class="fa fa-fw fa-user"></i> Clients</a>
+            <a href="#contact"><i class="fa fa-fw fa-envelope"></i> Contact</a>
+        </div>
     </div>
 </div>
