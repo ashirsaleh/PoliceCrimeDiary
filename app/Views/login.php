0b7e4903fc2cecb8daea9d25959b613438cdd9aa
Ashir <ashirsaleh@gmail.com>
2021-05-22 16:36:34 +0300

    some changes

--- app/Views/login.php
+++ app/Views/login.php
@@ -1,11 +1,15 @@
 <div class="main-div">
-    <h3>AUTHORIZATION</h3><br>
+    <div class="log-header">
+        <h3>AUTHORIZATION</h3><br>
+    </div>
     <div class="ch">
         <form action="" >
             <input type="policeid" id="policeId" placeholder="Police_ID"/>
             <input type="password" id="password" placeholder="Password"/>
+
+            <button  type="submit" class="btn">Accessing</button><br> 
+
         </form>
         
     </div> 
- <button  type="submit" class="btn">Accessing</button><br> 
 </div>
