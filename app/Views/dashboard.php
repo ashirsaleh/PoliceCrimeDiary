0fd7ed51aa8aa6a22e3c52bf39c79d39a4638e06
Ashir <ashirsaleh@gmail.com>
2021-05-24 12:59:08 +0300

    others

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -2,11 +2,19 @@
     <div id="mySidebar" class="sidebar">
         <div class='log'>
             <a href="#home"><i class="fa fa-fw fa-home"></i> Home</a>
-            <a href="#dasboard"><i class="fa fa-fw fa-dashboard"></i>Dashboard</a>
-            <a href="#form"><i class="fa fa-fw fa-document"></i>Form</a>
-            <a href="#services"><i class="fa fa-fw fa-wrench"></i> Services</a>
-            <a href="#clients"><i class="fa fa-fw fa-user"></i> Clients</a>
-            <a href="#contact"><i class="fa fa-fw fa-envelope"></i> Contact</a>
+            <a href="#dasboard"><i class="fa fa-fw fa-dashboard"></i>  Dashboard</a>
+            <div class="dropdown">
+                <a class="dropdown-toggle" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-file-text"></i>  Forms</a>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+            </div>
+                <a href="#services"><i class="fa fa-fw fa-wrench"></i> Services</a>
+                <a href="#clients"><i class="fa fa-fw fa-user"></i> Clients</a>
+                <a href="#contact"><i class="fa fa-fw fa-envelope"></i> Contact</a>
+                
         </div>
     </div>
 </div>
--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -1,5 +1,6 @@
 
-
-
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script type="text/javascript" src="assets/js/popover.js"></script>
+    <script type="text/javascript" src="assets/js/bootstrap.js"></script>
 </body>
 </html>
