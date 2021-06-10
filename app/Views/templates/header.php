b69a74a6115c313c80a0e39335d327d3c7ed7718
motinho311 <issamohammed311@gmail.com>
2021-06-10 20:47:10 +0300

    update user-profile

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -85,7 +85,7 @@
                 <!-- Sidebar user panel (optional) -->
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                     <div class="image">
-                        <img src="<?php echo base_url("assets/images/avatar.png"); ?>" class=" img-circle elevation-2"
+                        <img src="<?php echo base_url("assets/images/avatar.jpg"); ?>" class=" img-circle elevation-2"
                             alt="User Image">
                     </div>
                     <div class="info">
