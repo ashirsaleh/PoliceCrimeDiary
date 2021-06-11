2cf964e783f3b7b61d6970c364262cde4451c9f3
motinho311 <issamohammed311@gmail.com>
2021-06-12 00:10:12 +0300

    update user profile

--- app/Views/user-profile.php
+++ app/Views/user-profile.php
@@ -35,14 +35,14 @@
                                     <label for="inputName" class="col-sm-2 col-form-label">First Name</label>
                                     <div class="col-sm-10">
                                         <input type="email" class="form-control" id="inputName"
-                                            placeholder="First Name">
+                                            placeholder="<?php print_r($_SESSION['Fname']) ?>" disabled>
                                     </div>
                                 </div>
                                 <div class="form-group row">
                                     <label for="inputName2" class="col-sm-2 col-form-label">Last Name</label>
                                     <div class="col-sm-10">
                                         <input type="email" class="form-control" id="inputEmail"
-                                            placeholder="Last Name">
+                                            placeholder="<?php print_r($_SESSION['Lname']) ?> " disabled required>
                                     </div>
                                 </div>
                                 <div class="form-group row">
@@ -68,7 +68,7 @@
                                 </div>
                                 <div class="form-group row">
                                     <div class="offset-sm-2 col-sm-10">
-                                        <button type="submit" class="btn btn-danger">Submit</button>
+                                        <button type="submit" class="btn btn-primary">Change</button>
                                     </div>
                                 </div>
                             </form>
@@ -78,10 +78,39 @@
                     <!-- /.tab-content -->
                 </div><!-- /.card-body -->
             </div>
-            <!-- /.card -->
+            <div class="card card-primary">
+                <div class="card-header">
+                    <h3 class="card-title">About Me</h3>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body">
+                    <strong><i class="fas fa-book mr-1"></i> Full name</strong>
+
+                    <p class="text-muted">
+                    <p><?php echo session()->get('Fname'), ' ', session()->get('Lname') ?></p>
+                    </p>
+
+                    <hr>
+
+                    <strong><i class="fas fa-map-marker-alt mr-1"></i> Location</strong>
+
+                    <p class="text-muted">Dar Es Salaam,Tanzania</p>
+
+                    <hr>
+
+                    <strong><i class="fas fa-pencil-alt mr-1"></i> rank</strong>
+                    <p><?php print_r($_SESSION['rank']) ?></p>
+
+                    <hr>
+
+                    <strong><i class="far fa-file-alt mr-1"></i>Telephone</strong>
+                    <p><?php echo session()->get('tel_number') ?></p>
+
+                </div>
+                <!-- /.card -->
+            </div>
+            <!-- /.col -->
         </div>
-        <!-- /.col -->
-    </div>
-    <!-- /.row -->
+        <!-- /.row -->
     </div><!-- /.container-fluid -->
 </section>
