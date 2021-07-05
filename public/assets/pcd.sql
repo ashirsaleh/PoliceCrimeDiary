c4f7b7b377d12405b02f3e8b0f0523d3824a7af2
Ğ3ñě$ <genessambayena@gmail.com>
2021-07-05 11:34:26 +0300

    Update pcd.sql

--- public/assets/pcd.sql
+++ public/assets/pcd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jul 05, 2021 at 07:05 AM
+-- Generation Time: Jul 05, 2021 at 10:34 AM
 -- Server version: 10.3.29-MariaDB-0+deb10u1
 -- PHP Version: 7.3.27-1~deb10u1
 
@@ -55,9 +55,9 @@ CREATE TABLE `complainers` (
   `Fname` varchar(255) NOT NULL,
   `Lname` varchar(255) NOT NULL,
   `address` varchar(255) NOT NULL,
-  `date` date NOT NULL,
+  `date` datetime NOT NULL DEFAULT current_timestamp(),
   `natureComplaints` varchar(255) NOT NULL,
-  `phoneNum` int(50) NOT NULL,
+  `phoneNum` varchar(50) NOT NULL,
   `th_name` varchar(30) NOT NULL,
   `accusation` varchar(255) NOT NULL,
   `def_info` varchar(255) NOT NULL,
@@ -73,10 +73,10 @@ CREATE TABLE `complainers` (
 --
 
 INSERT INTO `complainers` (`complainerId`, `Fname`, `Lname`, `address`, `date`, `natureComplaints`, `phoneNum`, `th_name`, `accusation`, `def_info`, `other_info`, `type`, `p_number`, `color`, `w_statement`) VALUES
-(12, 'Shana Orr', 'Miriam Campos', 'Eum illum veniam q', '1971-10-22', 'Nisi enim non in mag', 0, 'Scott Blackburn', '', 'Inventore est accus', 'Minim sit dolore sun', '', 882, 'Nemo rerum qui id am', ''),
-(13, 'Megan Vaughn', 'Alden Pratt', 'Dolorum sint dolor ', '1981-12-22', 'Sit pariatur Nobis ', 0, 'Denise Todd', 'Fugit fugiat ullam', 'Facere cupiditate co', 'Magnam minim soluta ', '', 74, 'Dolorum est officia ', ''),
-(14, 'Whoopi Rowe', 'Daniel Valentine', 'In blanditiis vel ad', '1980-12-23', 'Aut sint sint harum', 0, 'Mona Mccullough', 'Quia corporis cupidi', 'Adipisci voluptatem ', 'Nostrum ea temporibu', '', 698, 'Velit non explicabo', ''),
-(15, 'MacKensie Chaney', 'Armand Carney', 'Id accusamus rerum s', '1986-10-06', 'Sed exercitationem u', 0, 'Stacy Hoffman', 'Nobis earum omnis vo', 'Dolore nulla explica', 'Repudiandae sit vero', '', 575, 'Elit est quo est s', '');
+(20, 'Keaton', 'Galena', 'Modi repudiandae fugiat nulla sunt sunt rem et molestiae quia sit esse', '2021-04-18 14:51:00', 'Aut tempora omnis delectus provident ex', '+255689756325', 'Jada Horton', 'Eum vel ratione proident velit aliqua Incididunt ipsum eos sunt sunt qui ea', 'Impedit est nostrum vitae aut', 'Non delectus magna deserunt nulla cupidatat voluptatem ab sit unde anim quas et quis enim ea eos', '', 363, 'Enim aut perspiciati', 'assets/uploads/documents/Screenshot from 2021-03-01 16-21-50_2.png'),
+(21, 'Abdul', 'Chancellor', 'Occaecat eos expedita eu fuga Fugiat magna ut fugiat ipsa occaecat ea eaque eos excepturi volup', '2021-06-11 17:43:00', 'Autem velit voluptatem reprehenderit minus eos sit vel ea qui delectus', '+255689756325', 'Phoebe Humphrey', 'Eos nulla et voluptatibus eveniet exercitation cumque itaque in labore molestias dolorem sint ali', 'Et aperiam dolor incidunt saepe cum quia do sed qui aliquip possimus minima beatae id', 'Recusandae Ullamco temporibus dolorem maiores velit cupidatat elit', '', 533, 'Sit magni sunt dolor', 'assets/uploads/documents/Screenshot from 2021-03-01 16-23-54.png'),
+(23, 'Omar Mcknight', 'Tasha Walsh', 'Modi inventore aut fugiat expedita et accusantium suscipit sequi et laborum sequi elit corporis se', '2020-01-06 17:43:00', 'Officiis natus aspernatur culpa placeat quis repudiandae animi', '+255689756325', 'Madison Bauer', 'Dolorem Nam in accusamus ipsum et cumque molestiae a ratione quo in do in voluptatem Reprehenderit', 'Natus nobis rerum rem non sit quo molestiae nisi et voluptate ad', 'Quis lorem laboris et commodi ex adipisicing eaque in velit pariatur Sed voluptas sit totam volupta', '', 729, 'Non omnis sint tempo', 'assets/uploads/documents/Screenshot from 2021-03-01 16-21-50_3.png'),
+(25, 'Kiona', 'Matthew', 'Facere officia et dolorem et inventore', '1979-12-18 05:22:00', 'Veritatis est dolor eaque sint cumque ea asperiores aspernatur aut mollit quam', '+255710234191', 'Clementine Reyes', 'Saepe rerum odit labore aliquid et ea ullamco et dolore maiores neque proident vero et sunt', 'Dicta error aut id quo et ut dolore nobis duis voluptatem blanditiis fugiat ut', 'Cupidatat dolorum saepe qui veniam veritatis', '', 887, 'Alias consectetur ma', 'assets/uploads/documents/Screenshot from 2021-03-01 16-23-54_2.png');
 
 -- --------------------------------------------------------
 
@@ -133,8 +133,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`policeId`, `policeNo`, `password`, `policepic`, `Fname`, `Lname`, `rank`, `age`, `tel_number`, `role`, `description`, `updated_at`) VALUES
-(21, '00321', '$2y$10$7f7IQZ85a9de558z48YVoeI7WJMY5laNO7nUWB97ORmp6Nl8dCShG', '', 'Hashir', 'Sales', 'Head Of Station', 0, '+255754552233', 0, 'The police office of  the stations\n                ', '2021-07-04 23:59:14'),
-(23, '00321', '$2y$10$7f7IQZ85a9de558z48YVoeI7WJMY5laNO7nUWB97ORmp6Nl8dCShG', '', 'Hashir', 'Sales', 'Head Of Station', 0, '+255754552233', 0, 'The police office of  the stations\n                ', '2021-07-04 23:59:14');
+(24, '00321', '$2y$10$V8z46EaE7nniKADaMxOQFeR/69bhz5NQ4jepc4VVPpy5bEZI3QGgy', '', 'Hashir', 'Saleh', 'Head of Station', 0, '+255754552233', 0, 'hashir saleh papa                ', '2021-07-05 02:37:53'),
+(25, '00654', '$2y$10$cBYTy76GY7/sOMXvYfzbHOjddSLRFhAQ1bf7QD/jTY22yhG9OCmx2', '', 'mohammed', 'issa', 'Police officer', 0, '+255754558899', 0, 'mohammed issa  hamad\n                ', '2021-07-05 02:39:21'),
+(27, '10033', '$2y$10$3erV7z4u7bmx81EKbHqKNO06B0.mHkv2F.u1s.jC.0m6b/EI21/NO', '', 'someuser', 'otheruser', 'Head of Station', 0, '+255754552233', 0, 'some other user is here here and here\n                \n                ', '2021-07-05 02:47:57');
 
 --
 -- Indexes for dumped tables
@@ -178,19 +179,19 @@ ALTER TABLE `cases`
 -- AUTO_INCREMENT for table `complainers`
 --
 ALTER TABLE `complainers`
-  MODIFY `complainerId` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `complainerId` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT for table `criminals`
 --
 ALTER TABLE `criminals`
-  MODIFY `accuserId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+  MODIFY `accuserId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `policeId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `policeId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
